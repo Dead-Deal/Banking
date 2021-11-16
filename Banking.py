ffbaca22ea3ae9cb2ebc8f7ab8b39c18ea88e140
Dead-Deal <94371736+Dead-Deal@users.noreply.github.com>
2021-11-16 11:22:26 +0530

    Option 4,1 Complete
    Creating a new account done

--- Banking.py
+++ Banking.py
@@ -5,9 +5,9 @@ import mysql.connector
 
 
 print("*"*15,"OUR BANK","*"*15)
-c=""
+option_1=""
 
-while c != 5:
+while option_1 != 5:
     print("\n")
     print("*"*15,"Menu:","*"*15)
     print("*"*15,"1)Deposit Money","*"*15)
@@ -17,29 +17,30 @@ while c != 5:
     print("*"*15,"5)Exit","*"*15)
 
     print()
-    c = int(input("Enter your choice:"))
+    option_1 = int(input("Enter your choice:"))
     print()
 
-    if c == 4:
-        print("*"*15,'1)Create account',"*"*15)
-        print("*"*15,'2)Modify account',"*"*15)
-        print("*"*15,'3)Delete account',"*"*15)
-        print("*"*15,'4)Back',"*"*15)
-        print()
-
-        mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database = "bank")
-        mycursor = mydb.cursor()
+    if option_1 == 4:
+        option_2 = ""
         
-        sc = ''
-        
-        while sc != 4:
-            sc = int(input('Enter your choice:'))
+        while option_2 != 4:
+            print("*"*15,'1)Create account',"*"*15)
+            print("*"*15,'2)Modify account',"*"*15)
+            print("*"*15,'3)Delete account',"*"*15)
+            print("*"*15,'4)Back',"*"*15)
             print()
-            if sc == 1:
+
+            mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database = "bank")
+            mycursor = mydb.cursor()
+        
+            option_2 = int(input('Enter your choice:'))
+            print()
+
+            if option_2 == 1:
                 res = ''.join(random.choices(string.ascii_letters + string.punctuation + string.digits, k = 8))
 
                 print('THANKS FOR CHOOSING OUR BANK')
@@ -50,9 +51,27 @@ while c != 5:
                 mn = int(input('Enter your mobile number:'))
                 rs = int(input('Initial Deposit:'))
                 no = int(input('Enter an Account number:'))
+                print()
                 
-                
-                sql = "INSERT INTO pin VALUES (%s, %s)"
-                val = [res,no]
+                mycursor.execute("SELECT Account_Number FROM info")
+                result = mycursor.fetchall()
+                acc_no_list = []
+                for results in result:
+                    for i in results:
+                        acc_no_list.append(i)
+                while no in acc_no_list:
+                    print('Account number already registered: ')
+                    no = int(input('Choose another Account number: '))
+                    print()
+                sql = "INSERT INTO info VALUES (%s, %s, %s, %s, %s)"
+                val = [res,nam,ad,mn,no]
                 mycursor.execute(sql,val)
                 mydb.commit()
+
+                pin = int(input('Enter a numeric pin/password: '))
+                print()
+                with open('pin_code.dat','ab') as fpin:
+                    x = {res, pin}
+                    pickle.dump(x,fpin)
+                print('CONGRATULATIONS!, Your Account has been CRAETED')
+                print()
